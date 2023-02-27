c150e178f3242481d92d19ea9c2b2e6197b0bb29
Jade Choate <jadeschoate@gmail.com>
2023-02-27 14:17:15 -0600

    phase4

--- Program.cs
+++ Program.cs
@@ -8,18 +8,26 @@ namespace Guessing_Game
         {
 
 int secretNumber = 42;
+// clear console
+Console.Clear();
 
             Console.WriteLine("Guess the secret number!");
+
+            for (int i = 0; i < 4; i++){
             string input = Console.ReadLine();
           //   Console.WriteLine(input);
 int parsedInput = int.Parse(input);
 
           if (parsedInput == secretNumber) {
-            Console.WriteLine("Your guess is correct!");
+            Console.WriteLine($"Your guess is correct! Guess number {i + 1}");
+            // stop loop if it matches
+            break;
           }
           else {
-            Console.WriteLine("Sorry your guess is wrong");
+            Console.WriteLine($"Sorry your guess is wrong. Guess number {i + 1}");
           }
+
+            }
         }
     }
 }
