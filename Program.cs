3e4513b48b203a9d4d50fe139feab18f22895f19
Jade Choate <jadeschoate@gmail.com>
2023-02-27 15:41:19 -0600

    phase 6

--- Program.cs
+++ Program.cs
@@ -1,39 +1,75 @@
 ﻿using System;
 
-namespace Guessing_Game
+namespace guessingGame
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             int secretNumber = new Random().Next(1, 100);
+            int y = 0;
             Console.WriteLine(secretNumber);
-            // clear console
-            //Console.Clear();
 
-            Console.WriteLine("Guess the secret number!");
+            Console.WriteLine("Please choose a difficulty level: 1, 2, 3");
+            Console.WriteLine("(1) Easy - this gives the user eight guesses.");
+            Console.WriteLine("(2) Medium - this gives the user six guesses.");
+            Console.WriteLine("(3) Hard - this gives the user four guesses.");
 
-            for (int i = 0; i < 4; i++)
+            string difficulty = Console.ReadLine();
+
+            if (difficulty == "1")
+            {
+                y = 8;
+            }
+            if (difficulty == "2")
+            {
+                y = 6;
+            }
+            if (difficulty == "3")
+            {
+                y = 4;
+            }
+            else if (difficulty == "Cheater")
+            {
+                y = 999;
+            }
+
+
+
+            // clears out the console beforehand
+            // Console.Clear();
+            Console.WriteLine("Guess the secret number");
+
+
+
+            for (int i = 0; i < y; i++)
+
             {
                 string input = Console.ReadLine();
-                //   Console.WriteLine(input);
+                // Console.WriteLine(input); No longer display the user's guess.
+
                 int parsedInput = int.Parse(input);
 
-                int guessesLeft = 4 - (i + 1);
+                int guessLeft = y - (i + 1);
 
                 if (parsedInput == secretNumber)
                 {
-                    Console.WriteLine($"Your guess is correct! Guess number {i + 1} ");
-                    // stop loop if it matches
+                    Console.WriteLine($"Your guess is correct.");
+                    // stops from looping
                     break;
                 }
-                else
+                if (parsedInput < secretNumber)
                 {
-                    Console.WriteLine($"Sorry your guess is wrong. Guess number {i + 1} You have {guessesLeft} guesses left");
+                    Console.WriteLine($"Your guess is too low. You have {guessLeft} guesses remaining.");
+                }
+                if (parsedInput > secretNumber)
+                {
+                    Console.WriteLine($"Your guess is too high. You have {guessLeft} guesses remaining.");
                 }
 
-            }
+            };
+
         }
+
     }
 }
