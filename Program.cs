48b7c4a6fff08bbbfa8c139aac80d3b6fceda037
Jade Choate <jadeschoate@gmail.com>
2023-02-27 13:51:54 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Guessing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+int secretNumber = 42;
+
+            Console.WriteLine("Guess the secret number!");
+            string input = Console.ReadLine();
+          //   Console.WriteLine(input);
+int parsedInput = int.Parse(input);
+
+          if (parsedInput == secretNumber) {
+            Console.WriteLine("Your guess is correct!");
+          }
+          else {
+            Console.WriteLine("Sorry your guess is wrong");
+          }
+        }
+    }
+}
